0f32be5a984773e5315e01fd947c10e94f99803d
thronestudio <contact@throne.work>
2023-02-01 11:18:41 -0800

    adding height range slider to hero block

--- assets/throne.js
+++ assets/throne.js
@@ -1,8 +1,25 @@
-$(function () {
 
-    var controller = new ScrollMagic.Controller();
-    // build scene
-    // var scene = new ScrollMagic.Scene({triggerElement: ".banner__content", duration: 600 })
-    //                 .setPin(".banner__content")
-    //                 .addTo(controller);
+$(function() {
+
+    // Works, but not with Flexbox
+    // var controller = new ScrollMagic.Controller();
+
+    // jQuery('.content-wrapper').each(function() {
+    //     var scene = new ScrollMagic.Scene({ 
+    //         triggerElement: this,
+    //         triggerHook: 0.5,
+    //         duration: '100%'}).setPin(this,  {pushfollowers: false}).addTo(controller);
+    // });
+
+    jQuery('.content-wrapper').each(function() {
+        let st = ScrollTrigger.create({
+            trigger: this,
+            pin: this,
+            start: "top center",
+            end: "50%",
+            pinSpacing: false
+        });
+    });
+
 });
+
